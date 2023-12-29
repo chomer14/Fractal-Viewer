49bfe4a98c791774227de10cccb7ff77ec6df142
chomer14 <80199862+chomer14@users.noreply.github.com>
2023-12-29 16:48:44 +0000

    Working fractal visualiser
    currently only does mandlebrot set

--- Fractal-Viewer/Form1.cs
+++ Fractal-Viewer/Form1.cs
@@ -17,34 +17,61 @@ namespace Fractal_Viewer
             InitializeComponent();
 
             Bitmap pbxBitmapBuilder = new Bitmap(fractalPbx.Width, fractalPbx.Height);
-            int centreX = 0, centreY = 0;
-            double scale = 1;
+            float centreX = -0.75f, centreY = 0;
+            float scale = 175;
+
+            int iterationMaximum = 50;
+            int magnitudeThresholdSquared = 2*2;
 
             for (int y = 0; y < fractalPbx.Height; y++)
             {
                 for (int x = 0; x < fractalPbx.Width; x++)
                 {
                     // start by finding what coords we are on the complex plane.
-                    double relativeX = x - fractalPbx.Width / 2;
-                    double relativeY = y - fractalPbx.Height / 2;
+                    float relativeX = x - fractalPbx.Width / 2;
+                    float relativeY = y - fractalPbx.Height / 2;
 
                     relativeX /= scale;
                     relativeY /= scale;
 
-                    relativeX -= centreX;
+                    relativeX += centreX;
                     relativeY += centreY;
+                    relativeY *= -1;
 
-                    double magnitude = Math.Sqrt(Math.Pow(relativeX, 2) + Math.Pow(relativeY, 2));
-                    double theta = Math.Atan2(relativeX, relativeY);
-                    theta += 2 * Math.PI;
-                    theta %= 2 * Math.PI;
-                    double thetaDeg = theta / Math.PI * 180;
+                    bool b = true;
+                    Color c = Color.White; // just a random default, has to be there or else compiler error
 
-                    // now we need to do the whole equation thing
+                    float cRe = relativeX;
+                    float cIm = relativeY;
 
-                    var rgbObject = HSVtoRGBConverter.HSVtoRGB(thetaDeg, 0.75, 0.75);
-                    Color colourFromRgb = Color.FromArgb(rgbObject.Red, rgbObject.Green, rgbObject.Blue);
-                    pbxBitmapBuilder.SetPixel(x, y, colourFromRgb);
+                    // these are correct
+                    float zRe = cRe;
+                    float zIm = cIm;
+
+                    for (int iterationCount = 0; iterationCount < iterationMaximum; iterationCount++)
+                    {
+                        // sqrt(a^2 + b^2) > r == a^2 + b^2 > r^2
+                        if ( (zRe*zRe + zIm*zIm) > magnitudeThresholdSquared )
+                        {
+                            var rgbObject = HSVtoRGBConverter.HSVtoRGB((float)iterationCount / (float)iterationMaximum * 360f, 1, 1);
+                            c = Color.FromArgb(rgbObject.Red, rgbObject.Green, rgbObject.Blue);
+                            b = false;
+                            break;
+                        }
+
+                        // square z and add c
+                        // (a+bi)^2 = a^2 + 2abi - b^2 = (a^2-b^2) + (2ab)i
+                        float tZIm = zIm;
+                        zIm = 2 * zRe * zIm + cIm;
+                        zRe = zRe * zRe - tZIm * tZIm + cRe;
+                    }
+
+                    if (b)
+                    {
+                        c = Color.Black;
+                    }
+
+                    pbxBitmapBuilder.SetPixel(x, y, c);
                 }
             }
 
