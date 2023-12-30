a9f70a016ad9af55b66489d8aa6b95725851a05a
chomer14 <80199862+chomer14@users.noreply.github.com>
2023-12-30 16:45:22 +0000

    Extract generate row function
    13.590

--- Fractal-Viewer/Form1.cs
+++ Fractal-Viewer/Form1.cs
@@ -8,11 +8,14 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Diagnostics;
+using System.Threading;
 
 namespace Fractal_Viewer
 {
     public partial class Form1 : Form
     {
+        public Bitmap pbxBitmapBuilder;
+
         public Form1()
         {
             InitializeComponent();
@@ -42,59 +45,64 @@ namespace Fractal_Viewer
 
         public Bitmap generateFractal(float centreX, float centreY, float scale,float iterationMaximum, float magnitudeThresholdSquared)
         {
-            Bitmap pbxBitmapBuilder = new Bitmap(fractalPbx.Width, fractalPbx.Height);
+            pbxBitmapBuilder = new Bitmap(fractalPbx.Width, fractalPbx.Height);
             for (int y = 0; y < fractalPbx.Height; y++)
             {
-                for (int x = 0; x < fractalPbx.Width; x++)
-                {
-                    // start by finding what coords we are on the complex plane.
-                    float relativeX = x - fractalPbx.Width / 2;
-                    float relativeY = y - fractalPbx.Height / 2;
-
-                    relativeX /= scale;
-                    relativeY /= scale;
-
-                    relativeX += centreX;
-                    relativeY += centreY;
-                    relativeY *= -1;
-
-                    bool b = true;
-                    Color c = Color.White; // just a random default, has to be there or else compiler error
-
-                    float cRe = relativeX;
-                    float cIm = relativeY;
-
-                    // these are correct
-                    float zRe = cRe;
-                    float zIm = cIm;
-
-                    for (int iterationCount = 0; iterationCount < iterationMaximum; iterationCount++)
-                    {
-                        // sqrt(a^2 + b^2) > r == a^2 + b^2 > r^2
-                        if ((zRe * zRe + zIm * zIm) > magnitudeThresholdSquared)
-                        {
-                            var rgbObject = HSVtoRGBConverter.HSVtoRGB((float)iterationCount / (float)iterationMaximum * 360f, 1, 1);
-                            c = Color.FromArgb(rgbObject.Red, rgbObject.Green, rgbObject.Blue);
-                            b = false;
-                            break;
-                        }
-
-                        // square z and add c
-                        // (a+bi)^2 = a^2 + 2abi - b^2 = (a^2-b^2) + (2ab)i
-                        float tZIm = zIm;
-                        zIm = 2 * zRe * zIm + cIm;
-                        zRe = zRe * zRe - tZIm * tZIm + cRe;
-                    }
-
-                    if (b)
-                    {
-                        c = Color.Black;
-                    }
-
-                    pbxBitmapBuilder.SetPixel(x, y, c);
-                }
+                generateFractalRow(centreX, centreY, scale, iterationMaximum, magnitudeThresholdSquared, y);
             }
             return pbxBitmapBuilder;
+        }
+
+        public void generateFractalRow(float centreX, float centreY, float scale, float iterationMaximum, float magnitudeThresholdSquared, int y)
+        {
+            for (int x = 0; x < fractalPbx.Width; x++)
+            {
+                // start by finding what coords we are on the complex plane.
+                float relativeX = x - fractalPbx.Width / 2;
+                float relativeY = y - fractalPbx.Height / 2;
+
+                relativeX /= scale;
+                relativeY /= scale;
+
+                relativeX += centreX;
+                relativeY += centreY;
+                relativeY *= -1;
+
+                bool b = true;
+                Color c = Color.White; // just a random default, has to be there or else compiler error
+
+                float cRe = relativeX;
+                float cIm = relativeY;
+
+                // these are correct
+                float zRe = cRe;
+                float zIm = cIm;
+
+                for (int iterationCount = 0; iterationCount < iterationMaximum; iterationCount++)
+                {
+                    // sqrt(a^2 + b^2) > r == a^2 + b^2 > r^2
+                    if ((zRe * zRe + zIm * zIm) > magnitudeThresholdSquared)
+                    {
+                        var rgbObject = HSVtoRGBConverter.HSVtoRGB((float)iterationCount / (float)iterationMaximum * 360f, 1, 1);
+                        c = Color.FromArgb(rgbObject.Red, rgbObject.Green, rgbObject.Blue);
+                        b = false;
+                        break;
+                    }
+
+                    // square z and add c
+                    // (a+bi)^2 = a^2 + 2abi - b^2 = (a^2-b^2) + (2ab)i
+                    float tZIm = zIm;
+                    zIm = 2 * zRe * zIm + cIm;
+                    zRe = zRe * zRe - tZIm * tZIm + cRe;
+                }
+
+                if (b)
+                {
+                    c = Color.Black;
+                }
+
+                pbxBitmapBuilder.SetPixel(x, y, c);
+            }
         }
     }
 }
