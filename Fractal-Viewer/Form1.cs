4da99e834a955676b23ae97fed70a3ca39e350e6
chomer14 <80199862+chomer14@users.noreply.github.com>
2023-12-31 12:57:43 +0000

    Factorising fractalGenerator

--- Fractal-Viewer/Form1.cs
+++ Fractal-Viewer/Form1.cs
@@ -16,9 +16,6 @@ namespace Fractal_Viewer
 
         float colourShift = 0;
 
-        double saturation = 1;
-        double brightness = 1;
-
         public Form1()
         {
             this.MouseWheel += Form1_Scroll;
@@ -46,83 +43,6 @@ namespace Fractal_Viewer
             commandPnl.Height = b.Height;
 
             fractalPbx.Image = b;
-        }
-
-        public Bitmap generateFractal()
-        {
-            List<List<Color>> colourRows = new List<List<Color>>();
-
-            for (int y = 0; y < fractalPbx.Height; y++)
-            {
-                colourRows.Add(generateFractalRow(y));
-            }
-
-            Bitmap pbxBitmapBuilder = new Bitmap(fractalPbx.Width, fractalPbx.Height);
-
-            for (int i = 0; i < colourRows.Count; i++)
-            {
-                for (int j = 0; j < colourRows[i].Count; j++)
-                {
-                    pbxBitmapBuilder.SetPixel(j, i, colourRows[i][j]);
-                }
-            }
-
-            return pbxBitmapBuilder;
-        }
-
-        public List<Color> generateFractalRow(int y)
-        {
-            List<Color> colourList = new List<Color>();
-
-            for (int x = 0; x < fractalPbx.Width; x++)
-            {
-                // start by finding what coords we are on the complex plane.
-                float relativeX = x - fractalPbx.Width / 2;
-                float relativeY = y - fractalPbx.Height / 2;
-
-                relativeX /= scale;
-                relativeY /= scale;
-
-                relativeX += centreX;
-                relativeY += centreY;
-                relativeY *= -1;
-
-                bool b = true;
-                Color c = Color.White; // just a random default, has to be there or else compiler error
-
-                float cRe = relativeX;
-                float cIm = relativeY;
-
-                // these are correct
-                float zRe = cRe;
-                float zIm = cIm;
-
-                for (int iterationCount = 0; iterationCount < iterationMaximum; iterationCount++)
-                {
-                    // sqrt(a^2 + b^2) > r == a^2 + b^2 > r^2
-                    if ((zRe * zRe + zIm * zIm) > magnitudeThresholdSquared)
-                    {
-                        var rgbObject = HSVtoRGBConverter.HSVtoRGB(((float)iterationCount / (float)iterationMaximum * 360f + colourShift) % 360f, saturation, brightness);
-                        c = Color.FromArgb(rgbObject.Red, rgbObject.Green, rgbObject.Blue);
-                        b = false;
-                        break;
-                    }
-
-                    // square z and add c
-                    // (a+bi)^2 = a^2 + 2abi - b^2 = (a^2-b^2) + (2ab)i
-                    float tZIm = zIm;
-                    zIm = 2 * zRe * zIm + cIm;
-                    zRe = zRe * zRe - tZIm * tZIm + cRe;
-                }
-
-                if (b)
-                {
-                    c = Color.Black;
-                }
-
-                colourList.Add(c);
-            }
-            return colourList;
         }
 
         private void Form1_Scroll(object sender, MouseEventArgs e)
---
+++ Fractal-Viewer/fractalGenerator.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.AxHost;
+
+namespace Fractal_Viewer
+{
+    public static class fractalGenerator
+    {
+
+        static public Bitmap generateFractal(Bitmap fractalPbx, float scale, float centreX, float centreY, int iterationMaximum, int magnitudeThresholdSquared, float colourShift)
+        {
+            List<List<Color>> colourRows = new List<List<Color>>();
+
+            for (int y = 0; y < fractalPbx.Height; y++)
+            {
+                colourRows.Add(generateFractalRow(y, fractalPbx, scale, centreX, centreY, iterationMaximum, magnitudeThresholdSquared, colourShift));
+            }
+
+            Bitmap pbxBitmapBuilder = new Bitmap(fractalPbx.Width, fractalPbx.Height);
+
+            for (int i = 0; i < colourRows.Count; i++)
+            {
+                for (int j = 0; j < colourRows[i].Count; j++)
+                {
+                    pbxBitmapBuilder.SetPixel(j, i, colourRows[i][j]);
+                }
+            }
+
+            return pbxBitmapBuilder;
+        }
+
+        static public List<Color> generateFractalRow(int y, Bitmap fractalPbx, float scale, float centreX, float centreY, int iterationMaximum, int magnitudeThresholdSquared, float colourShift)
+        {
+            List<Color> colourList = new List<Color>();
+
+            for (int x = 0; x < fractalPbx.Width; x++)
+            {
+                // start by finding what coords we are on the complex plane.
+                float relativeX = x - fractalPbx.Width / 2;
+                float relativeY = y - fractalPbx.Height / 2;
+
+                relativeX /= scale;
+                relativeY /= scale;
+
+                relativeX += centreX;
+                relativeY += centreY;
+                relativeY *= -1;
+
+                bool b = true;
+                Color c = Color.White; // just a random default, has to be there or else compiler error
+
+                float cRe = relativeX;
+                float cIm = relativeY;
+
+                // these are correct
+                float zRe = cRe;
+                float zIm = cIm;
+
+                for (int iterationCount = 0; iterationCount < iterationMaximum; iterationCount++)
+                {
+                    // sqrt(a^2 + b^2) > r == a^2 + b^2 > r^2
+                    if ((zRe * zRe + zIm * zIm) > magnitudeThresholdSquared)
+                    {
+                        var rgbObject = HSVtoRGBConverter.HSVtoRGB(((float)iterationCount / (float)iterationMaximum * 360f + colourShift) % 360f, saturation, brightness);
+                        c = Color.FromArgb(rgbObject.Red, rgbObject.Green, rgbObject.Blue);
+                        b = false;
+                        break;
+                    }
+
+                    // square z and add c
+                    // (a+bi)^2 = a^2 + 2abi - b^2 = (a^2-b^2) + (2ab)i
+                    float tZIm = zIm;
+                    zIm = 2 * zRe * zIm + cIm;
+                    zRe = zRe * zRe - tZIm * tZIm + cRe;
+                }
+
+                if (b)
+                {
+                    c = Color.Black;
+                }
+
+                colourList.Add(c);
+            }
+            return colourList;
+        }
+    }
+}
