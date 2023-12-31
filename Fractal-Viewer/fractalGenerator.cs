6823ffc275bcf1aafb2fd1904a65cca7d85e6b03
chomer14 <80199862+chomer14@users.noreply.github.com>
2023-12-31 13:00:51 +0000

    Update fractalGenerator.cs

--- Fractal-Viewer/fractalGenerator.cs
+++ Fractal-Viewer/fractalGenerator.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using static System.Windows.Forms.AxHost;
 
 namespace Fractal_Viewer
 {
